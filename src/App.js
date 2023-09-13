b221d3195802cf927c16f66ae76dc1db85b9b6d3
sps@#254 <suryapratap2542@gmail.com>
2023-09-13 15:39:52 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Testimonials from './components/Testimonials';
+import reviews from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-sky-200'>
+      <div className='text-center'> 
+        <h1 className='text-4xl font-bold'>Our Testimonials</h1>
+        <div className='bg-red-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
+        < Testimonials reviews={reviews}/>
+      </div>
     </div>
-  );
-}
+  )
+  }
 
 export default App;
